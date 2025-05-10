f464bd0a333f9e8e181b5a58ec467c2fd012e52b
María Camila Cubides Martín <mc.cubides@uniandes.edu.co>
2025-05-09 19:18:12 -0500

    Modificaciones

---
+++ e2e/misw-4103-kraken/krakenRunner.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+const { spawnSync } = require('child_process');
+
+const subfolder = process.argv[2] || 'escenarios';
+const escenariosDir = path.join('./', subfolder);
+const featureTarget = './features/Example.feature';
+const stepsTarget = './features/web/step_definitions/step.js';
+
+// Recorre cada subcarpeta
+const funcionalidades = fs.readdirSync(escenariosDir).filter(f => fs.statSync(path.join(escenariosDir, f)).isDirectory());
+
+funcionalidades.sort((a, b) => {
+  if (a === 'crear_sitio') return -1;
+  if (b === 'crear_sitio') return 1;
+  return 0;
+});
+
+let allScenarios = [];
+
+funcionalidades.forEach(func => {
+  const funcPath = path.join(escenariosDir, func);
+  const stepPath = path.join(funcPath, 'step.js');
+  const features = fs.readdirSync(funcPath).filter(f => f.endsWith('.feature'));
+
+  features.forEach(feature => {
+    allScenarios.push({
+      featurePath: path.join(funcPath, feature),
+      stepPath,
+      name: `${func}/${feature}`
+    });
+  });
+});
+
+for (const scenario of allScenarios) {
+  console.log('='.repeat(80));
+  console.log(`Running scenario: ${scenario.name}`);
+  console.log('='.repeat(80));
+
+  try {
+    fs.copyFileSync(scenario.featurePath, featureTarget);
+    fs.copyFileSync(scenario.stepPath, stepsTarget);
+  } catch (err) {
+    console.error(`Error copying files for scenario ${scenario.name}:`, err);
+    continue;
+  }
+
+  const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+  const res = spawnSync(npmCmd, ['run', 'test'], { stdio: 'inherit' , shell: true});
+
+  if (res.status !== 0) {
+    console.error(`Scenario ${scenario.name} failed.`);
+  } else {
+    console.log(`Scenario ${scenario.name} passed.`);
+  }
+}
--- e2e/misw-4103-kraken/takeScreenshot.js
+++ e2e/misw-4103-kraken/takeScreenshot.js
@@ -2,13 +2,12 @@ const fs = require('fs');
 const config = require('./properties.json');
 
 const takeKrakenScreenshot = async (driver, scenario, step) => {
-    let date = (new Date()).toISOString().split('T')[0];
     let ghostVersion = config.GHOST_VERSION;
-    if (!fs.existsSync(`./screenshots/${date}/v${ghostVersion}`)) {
-        fs.mkdirSync(`./screenshots/${date}/v${ghostVersion}`, { recursive: true });
+    if (!fs.existsSync(`./screenshots/v${ghostVersion}`)) {
+        fs.mkdirSync(`./screenshots/v${ghostVersion}`, { recursive: true });
     }
 
-    await driver.saveScreenshot(`./screenshots/${date}/v${ghostVersion}/${scenario}_${step}.png`);
+    await driver.saveScreenshot(`./screenshots/v${ghostVersion}/${scenario}_${step}.png`);
 
 }
 
